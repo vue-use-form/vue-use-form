01cff85dc1834417f8c3483d1aa9afa165a7192e
snowingfox <1503401882@qq.com>
2022-05-24 11:18:28 +0800

    :construction: new example

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { computed, watch, watchEffect } from 'vue'
 import { useForm } from '../../packages/core/src/index'
 
 interface Inputs {
   username: string
+  password: string
+  showEmail: boolean
+  email: string
 }
 
 const {
@@ -10,12 +14,14 @@ const {
   register,
   createSubmitHandler,
   createErrorHandler,
+  reset,
   handleSubmit,
 } = useForm<Inputs>({
   mode: 'onSubmit',
+  shouldFocusError: true,
 })
 
-const usernameField = register('username', {
+const [usernameField, elRef] = register('username', {
   required: 'Username is required',
   validate: {
     isStartWithAt: async (val) => {
@@ -30,6 +36,14 @@ const usernameField = register('username', {
   },
 })
 
+const [showEmailField, showEmailRef] = register('showEmail', {
+  required: 'Please check the checkbox',
+})
+
+const [emailField, emailRef] = register('email', {
+  required: 'Email is required',
+})
+
 const onSubmit = createSubmitHandler((data) => {
   console.log(data)
 })
@@ -37,12 +51,35 @@ const onSubmit = createSubmitHandler((data) => {
 const onErrors = createErrorHandler((errors) => {
   console.log(errors)
 })
+
+function resetField() {
+  reset({
+    username: '114514',
+  }, {
+    keepIsSubmitted: false,
+    keepSubmitCount: false,
+    keepErrors: false,
+    keepDirty: true,
+  })
+}
+
 </script>
 
 <template>
-  {{ formState }}
-  <el-input v-model="usernameField" />
+  <pre>
+    <code>
+      {{ formState }}
+    </code>
+    {{ emailField }}
+  </pre>
+
+  <el-switch ref="showEmailRef" v-model="showEmailField" />
+  <el-input ref="elRef" v-model="usernameField" />
+  <template v-if="showEmailField">
+    <el-input ref="emailRef" v-model="emailField" />
+  </template>
   <el-button type="primary" @click="handleSubmit(onSubmit, onErrors)()" v-text="'Submit'" />
+  <el-button type="primary" @click="resetField" v-text="'resetField'" />
 </template>
 
 <style>
