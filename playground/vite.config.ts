923a42d9d13db110d620505d60fe7e0378069992
snowingfox <1503401882@qq.com>
2022-08-04 17:51:47 +0800

    :wrench: import v-form

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { transformAssetUrls } from '@quasar/vite-plugin'
-import Unocss from 'unocss/vite'
+import VForm from '@vue-use-form/v-form/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VForm(),
     vue({
       template: { transformAssetUrls },
     }),
-    Unocss({}),
   ],
 })
