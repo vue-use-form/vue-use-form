f4c81515f6bfce23fb9e3a4ecf0bb7494879486b
snowingfox <1503401882@qq.com>
2022-10-18 21:31:20 +0800

    :fire: ensure elRef is Object before get keys

--- packages/core/src/utils/getFormEl.ts
+++ packages/core/src/utils/getFormEl.ts
@@ -2,19 +2,19 @@ import { unref } from 'vue'
 import type { MaybeRef } from '../types/utils'
 import type { FieldElement } from '../types/filed'
 import { isFieldElement } from './isFieldElement'
-import { isHTMLElement } from './index'
+import { isHTMLElement, isObject } from './index'
 
 const getValidFormElement = (el: HTMLElement) => el.querySelectorAll('input, textarea, select')[0] as FieldElement
 
 export function getFormEl(elRef: MaybeRef<any> | InputEvent): FieldElement | undefined {
   elRef = unref(elRef)
-  const keys = Reflect.ownKeys(elRef)
 
   if (isFieldElement(elRef)) {
     return elRef
   } else if (isHTMLElement(elRef)) {
     return getValidFormElement(elRef)
-  } else {
+  } else if (isObject(elRef)) {
+    const keys = Reflect.ownKeys(elRef)
     for (const key of keys) {
       const val = unref(elRef[key])
       if (isFieldElement(val)) {
