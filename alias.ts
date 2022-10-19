165244ed9485306d01798fc4c4da4ccf12838a57
snowingfox <1503401882@qq.com>
2022-10-19 15:08:44 +0800

    :wrench: config path

--- alias.ts
+++ alias.ts
@@ -3,5 +3,7 @@ import { resolve } from 'path'
 const r = (p: string) => resolve(__dirname, p)
 
 export const alias: Record<string, string> = {
-  '@vue-use-form/core': r('./packages/core/src/'),
+  'vue-use-form': r('./packages/core/src/'),
+  '@vue-use-form/yup': r('./packages/resolver-yup/src/index.ts'),
+  '@vue-use-form/class-validator': r('./packages/resolver-class-validator/src/index.ts'),
 }
