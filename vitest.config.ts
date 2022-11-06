3437d519ed5e3db2137d6dd77bc23013a24bab81
snowingfox <1503401882@qq.com>
2022-11-06 22:56:48 +0800

    :zap: use vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
-import type { UserConfigExport } from 'vitest/config'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-import Vue from '@vitejs/plugin-vue'
+// import Vue from '@vitejs/plugin-vue'
 import { alias } from './alias'
 
 export default defineConfig({
   optimizeDeps: {
     entries: [],
   },
-  plugins: [Vue()],
+  // plugins: [Vue()],
   resolve: {
     alias,
   },
-  test: {
-    environment: 'jsdom',
-  },
-} as UserConfigExport &{ test: any })
+})
