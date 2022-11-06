e0cb5655eafafc42e4d4024d4a6ea8f1c471affa
snowingfox <1503401882@qq.com>
2022-11-06 20:20:09 +0800

    :sparkles: playground

--- test/components/Login.vue
+++ test/components/Login.vue
@@ -7,14 +7,14 @@ interface Inputs {
 }
 
 const { formState, register } = useForm<Inputs>()
-
-const [usernameField] = register('username', {
-  required: true,
-  maxLength: 10,
-})
 </script>
 
 <template>
   <input :value="formState">
-  <input v-model="usernameField">
+  <input
+    :="register('username', {
+  required: true,
+  maxLength: 10,
+})"
+  >
 </template>
